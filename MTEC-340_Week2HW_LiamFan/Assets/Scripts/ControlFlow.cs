309ae6b9a0cdd7108fae4b30a637f819c314c3fc
l-fan3 <lfan3@berklee.edu>
2025-02-01 16:05:53 -0500

    Created Bool and Loop within script. Added images and URL to md file.

--- MTEC-340_Week2HW_LiamFan/Assets/Scripts/ControlFlow.cs
+++ MTEC-340_Week2HW_LiamFan/Assets/Scripts/ControlFlow.cs
@@ -1,11 +1,32 @@
+using System;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ControlFlow : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public bool flag = false;
+    public int powerValue = 10;
+    public int baseValue = 2;
+        
     void Start()
     {
+        //Flag Boolean
+        if (flag is true)
+        {
+            Debug.Log("Boolean flag is set");
+        }
+
+        else
+        {
+            Debug.Log("Boolean flag isn't set");
+        }
         
+        //Expo Loop 
+        for (int i = 1; i <= powerValue; i++)
+        {
+            Debug.Log($"The {i} power of {baseValue} is {Math.Pow(baseValue, i)}");
+        }
     }
 
     // Update is called once per frame
---
+++ README.md
@@ -0,0 +1,9 @@
+## Image 1 (Flag True)
+![1](Images/MTEC-340_Week2HW.1_LiamFan.png)
+## Image 2 (Flag False)
+![2](Images/MTEC-340_Week2HW.2_LiamFan.png)
+
+--
+
+I don't know if it's possible to have a favourite game OST but I played a lot in survival mode so I have the OST Soundtrack of [The Long Dark](https://www.youtube.com/watch?v=FuwKyburg4o) stuck in my head.
+
